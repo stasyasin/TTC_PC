5e34cfba4ce5f63b37b4cc55daacadaa5d76251d
stasyasin <stasyasin@gmail.com>
2016-08-30 23:40:22 +0200

    deleting (commented) f@#cking envilope

--- app/src/main/java/pointscalculator/ttr_pc/MainActivity.java
+++ app/src/main/java/pointscalculator/ttr_pc/MainActivity.java
@@ -38,15 +38,15 @@ public class MainActivity extends AppCompatActivity
         setSupportActionBar(toolbar);
         mSettings = PreferenceManager.getDefaultSharedPreferences(this);
 
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
